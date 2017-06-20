90273d47f75dced818d158e4de2020ada3e2cc06
Maro <grafika.marekduda@gmail.com>
2017-06-20 21:12:33 +0200

    added new function

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
-function Phone(brand, price, color) {
-    this.brand = brand; 
+function Phone(brand, model, price, color) {
+    this.brand = brand;
+    this.model = model;
     this.price = price;
     this.color = color;
 }
@@ -8,11 +9,16 @@ Phone.prototype.printInfo = function() {
     console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
 }
 
-var samsungGalaxyS6 = new Phone("Samsung", 2150, "black");
-var iphone6S = new Phone("Apple", 2250, "silver");
-var onePlusOne = new Phone("OnePlus", 1900, "red");
+Phone.prototype.alertInfo = function() {
+    alert("Wygrałeś telefon " + this.brand + " " + this.model + " !");
+}
+
+var samsungGalaxyS6 = new Phone("Samsung", "Galaxy S6", 2150, "black");
+var iphone6S = new Phone("Apple", "6S", 2250, "silver");
+var onePlusOne = new Phone("OnePlus", "One", 1900, "red");
 
 
 samsungGalaxyS6.printInfo();
 iphone6S.printInfo();
 onePlusOne.printInfo();
+samsungGalaxyS6.alertInfo();
